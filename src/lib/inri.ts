da3e722f46e27360d3e6fb21f87f1ecda98be4bd
INRI CHAIN <contact@inri.life>
2026-03-05 11:32:55 -0300

    Create inri.ts

---
+++ src/lib/inri.ts
@@ -0,0 +1,12 @@
+export const INRI = {
+  name: "INRI Chain",
+  chainIdDec: 3777,
+  chainIdHex: "0xEC1", // 3777 em hex
+  rpcUrl: "https://rpc-chain.inri.life",
+  symbol: "INRI",
+  explorerUrl: "https://explorer.inri.life", // <-- TROQUE se seu explorer for outro
+  website: "https://inrichain.github.io/inri-site/",
+  twitter: "https://x.com/", // opcional
+  telegram: "https://t.me/", // opcional
+  whitepaperUrl: "https://inri.life/whitepaper.pdf", // <-- TROQUE
+};
